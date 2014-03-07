c45c761452ee4f4271b41b0d6dd199e7ca79594f
tedy.saputro <tedy.saputro@multipolar.com>
2014-03-08 00:25:53 +0700

    mainUI

--- Stopwatch/src/net/rt/stopwatch/view/UIFrame.java
+++ Stopwatch/src/net/rt/stopwatch/view/UIFrame.java
@@ -8,6 +8,7 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Enumeration;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -20,6 +21,8 @@ import javax.swing.JTextField;
 import javax.swing.Timer;
 import javax.swing.WindowConstants;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableColumnModel;
 import javax.swing.table.TableModel;
 import javax.swing.SwingUtilities;
 
@@ -54,7 +57,10 @@ public class UIFrame extends javax.swing.JFrame {
 	private JButton startButton;
 	private JButton stopButton;
 	private JButton resetButton;
-	
+	private JButton repairButton;
+	private JButton reposisiButton;
+	private JButton waitDSButton;
+
 	private JLabel timeWindow;
 	private Timer timer;
 	private Timer ledTimer;
@@ -63,7 +69,7 @@ public class UIFrame extends javax.swing.JFrame {
 	private ImageIcon ledOn;
 	private JScrollPane jScrollPane1;
 	private JTable jTableMain;
-	private JPanel jPanelTable;
+	private TableColumn column;
 	private ImageIcon ledOff;
 	private JTextField jTextFieldSite;
 	private JTextField jTextFieldKodeUnit;
@@ -77,9 +83,12 @@ public class UIFrame extends javax.swing.JFrame {
 	private JPanel jPanelID;
 	private JPanel jPanelControl;
 	private JPanel jPanelTop;
-	private String[] judul= {"No","Digging", "Swing Loaded", "Dumping", "SwingUnloaded", "CYCLE TIME"};
+	private JPanel jPanelTable;
+
+	private String[] judul= {"No","Digging", "Swing Loaded", "Dumping", "SwingUnloaded", "CYCLE TIME", "WAIT DT","REPOSISI", "REPAIR FRONT"};
 	private Object[][] data= {};
-	private int kolom=1;
+	private int kolom=0;
+	private int baris=0;
 	/**
 	* Auto-generated main method to display this JFrame
 	*/
@@ -113,22 +122,22 @@ public class UIFrame extends javax.swing.JFrame {
 			jPanelTop = new JPanel();
 			GridBagLayout jPanelTopLayout = new GridBagLayout();
 			getContentPane().add(jPanelTop, new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
-			jPanelTopLayout.rowWeights = new double[] {0.1, 0.1, 0.1};
-			jPanelTopLayout.rowHeights = new int[] {20, 20, 7};
+			jPanelTopLayout.rowWeights = new double[] {0.0, 0.1, 0.1};
+			jPanelTopLayout.rowHeights = new int[] {48, 20, 7};
 			jPanelTopLayout.columnWeights = new double[] {0.0, 0.1};
-			jPanelTopLayout.columnWidths = new int[] {300, 7};
+			jPanelTopLayout.columnWidths = new int[] {424, 7};
 			jPanelTop.setLayout(jPanelTopLayout);
 			{
 				timeWindow = new JLabel(stopWatch.toString());
 				jPanelTop.add(timeWindow, new GridBagConstraints(0, 2, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
-				timeWindow.setFont(new Font("Calibri", Font.BOLD, 32)); 
+				timeWindow.setFont(new java.awt.Font("Calibri",1,72)); 
 				timeWindow.setForeground(new java.awt.Color(255,128,0));
-				timeWindow.setPreferredSize(new java.awt.Dimension(229, 97));
+				timeWindow.setPreferredSize(new java.awt.Dimension(300, 150));
 			}
 			{
 				jPanelControl = new JPanel();
 				GridBagLayout jPanelControlLayout = new GridBagLayout();
-				jPanelControlLayout.columnWidths = new int[] {183, 7};
+				jPanelControlLayout.columnWidths = new int[] {172, 7};
 				jPanelControlLayout.rowHeights = new int[] {7, 7, 7, 7};
 				jPanelControlLayout.columnWeights = new double[] {0.0, 0.1};
 				jPanelControlLayout.rowWeights = new double[] {0.1, 0.1, 0.1, 0.1};
@@ -137,20 +146,20 @@ public class UIFrame extends javax.swing.JFrame {
 				{
 					stopButton = new JButton("Stop");
 					jPanelControl.add(stopButton, new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
-					stopButton.setPreferredSize(new java.awt.Dimension(100, 30));
+					stopButton.setPreferredSize(new java.awt.Dimension(120, 30));
 					stopButton.setSize(200, 30);
 					stopButton.addActionListener(listener);
 				}
 				{
 					startButton = new JButton("Start");
 					jPanelControl.add(startButton, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
-					startButton.setPreferredSize(new java.awt.Dimension(100, 30));
+					startButton.setPreferredSize(new java.awt.Dimension(120, 30));
 					startButton.addActionListener(listener);
 				}
 				{
 					resetButton = new JButton("Reset");
-					jPanelControl.add(resetButton, new GridBagConstraints(0, 3, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
-					resetButton.setPreferredSize(new java.awt.Dimension(100, 30));
+					jPanelControl.add(resetButton, new GridBagConstraints(0, 2, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
+					resetButton.setPreferredSize(new java.awt.Dimension(120, 30));
 					resetButton.setSize(100, 30);
 					resetButton.addActionListener(listener);
 				}
@@ -162,11 +171,29 @@ public class UIFrame extends javax.swing.JFrame {
 				}
 				{
 					splitButton = new JButton();
-					jPanelControl.add(splitButton, new GridBagConstraints(0, 2, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
-					splitButton.setText("Split");
-					splitButton.setPreferredSize(new java.awt.Dimension(100, 30));
+					jPanelControl.add(splitButton, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
+					splitButton.setText("SPLIT");
+					splitButton.setPreferredSize(new java.awt.Dimension(120, 30));
 					splitButton.addActionListener(listener);
 
+				}
+				{
+					waitDSButton = new JButton();
+					jPanelControl.add(waitDSButton, new GridBagConstraints(1, 1, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
+					waitDSButton.setText("WAIT DS");
+					waitDSButton.setPreferredSize(new java.awt.Dimension(120, 30));
+				}
+				{
+					reposisiButton = new JButton();
+					jPanelControl.add(reposisiButton, new GridBagConstraints(1, 2, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
+					reposisiButton.setText("REPOSISI");
+					reposisiButton.setPreferredSize(new java.awt.Dimension(120, 30));
+				}
+				{
+					repairButton = new JButton();
+					jPanelControl.add(repairButton, new GridBagConstraints(1, 3, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
+					repairButton.setText("REPAIR FRONT");
+					repairButton.setPreferredSize(new java.awt.Dimension(120, 30));
 				}
 			}
 			{
@@ -222,26 +249,47 @@ public class UIFrame extends javax.swing.JFrame {
 		}
 		{
 			jPanelTable = new JPanel();
-			GridBagLayout jPanelTableLayout = new GridBagLayout();
-			jPanelTableLayout.columnWidths = new int[] {7, 7, 7};
-			jPanelTableLayout.rowHeights = new int[] {7};
-			jPanelTableLayout.columnWeights = new double[] {0.0, 0.1, 0.0};
-			jPanelTableLayout.rowWeights = new double[] {0.1};
-			getContentPane().add(jPanelTable, new GridBagConstraints(0, 2, 1, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
-			jPanelTable.setLayout(jPanelTableLayout);
+			getContentPane().add(jPanelTable, new GridBagConstraints(0, 2, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
+		}
+		{
+			jScrollPane1 = new JScrollPane();
+			getContentPane().add(jScrollPane1, new GridBagConstraints(0, 3, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
 			{
-				jScrollPane1 = new JScrollPane();
-				jPanelTable.add(jScrollPane1, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
-				{
-					TableModel jTableMainModel = 
-							new DefaultTableModel(data, judul);
-					jTableMain = new JTable();
-					jScrollPane1.setViewportView(jTableMain);
-					jTableMain.setModel(jTableMainModel);
-				}
+				TableModel jTableMainModel = new DefaultTableModel(data, judul);
+				jTableMain = new JTable();
+				
+				
+				jScrollPane1.setViewportView(jTableMain);
+				jTableMain.setModel(jTableMainModel);
+				//					jTableMain.getColumnModel().getColumn(0).setMaxWidth(40);
+				//					jTableMain.getColumnModel().getColumn(1).setMaxWidth(100);
+				//					jTableMain.getColumnModel().getColumn(2).setMaxWidth(100);
+				//					jTableMain.getColumnModel().getColumn(3).setMaxWidth(100);
+				//					jTableMain.getColumnModel().getColumn(4).setMaxWidth(100);
+				//					jTableMain.getColumnModel().getColumn(5).setMaxWidth(100);
+				//					jTableMain.getColumnModel().getColumn(6).setMaxWidth(100);
+				//					jTableMain.getColumnModel().getColumn(7).setMaxWidth(100);
+				//					jTableMain.getColumnModel().getColumn(8).setMaxWidth(100);
+				
+				
 			}
 		}
 	}
+	
+//	private TableColumnModel setColumnTableSize(TableColumnModel columnmodel){
+//		columnmodel.getColumn(0).setPreferredWidth(5);
+//		columnmodel.getColumn(1).setPreferredWidth(5);
+//		columnmodel.getColumn(2).setPreferredWidth(5);
+//		columnmodel.getColumn(3).setPreferredWidth(5);
+//		columnmodel.getColumn(4).setPreferredWidth(5);
+//		columnmodel.getColumn(5).setPreferredWidth(5);
+//		columnmodel.getColumn(6).setPreferredWidth(5);
+//		columnmodel.getColumn(7).setPreferredWidth(5);
+//		columnmodel.getColumn(8).setPreferredWidth(5);
+//
+//		
+//		return columnmodel;
+//	}
 	private void initGUI() {
 		try {
 			GridBagLayout thisLayout = new GridBagLayout();
@@ -253,7 +301,7 @@ public class UIFrame extends javax.swing.JFrame {
 			getContentPane().setLayout(thisLayout);
 			initStopWatch();
 			pack();
-			this.setSize(793, 550);
+			this.setSize(981, 634);
 		} catch (Exception e) {
 		    //add your error handling code here
 			e.printStackTrace();
@@ -313,23 +361,33 @@ public class UIFrame extends javax.swing.JFrame {
 	}
 	
 	private void updateTable(String value){
-		
-		Object [][] temp = new Object[data.length+1][6];
+		//jumlah array 1
+		Object [][] temp = new Object[baris+1][6];
 		
 		//inisialisasi
 		for(int brs=0;brs<data.length;brs++)
+
 		{
-			for(int klm=0;klm<4;klm++)
+			for(int klm=0;klm<6;klm++)
 			{
 				temp[brs][klm]=data[brs][klm];
 			}
 		}		
-		//fill data
-		kolom++;
-		temp[data.length][kolom]=value;
+		if(kolom==0){
+			temp[baris][kolom]=baris+1;
+			kolom++;
+		}
+		//array ke satu=0
+		temp[baris][kolom]=value;
 
 		data=temp;
 		jTableMain.setModel(new DefaultTableModel(data, judul));
-
+		
+		if(kolom<5){
+			kolom++;
+		}else{
+			kolom=0;
+			baris++;
+		}
 	}
 }
